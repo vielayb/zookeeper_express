5a4991e5a3358353daebcb512548c584dd73bb21
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2022-03-30 20:02:53 -0400

    Post data validated

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 // this is the main file that our server will run from
 
+const fs = require('fs');
+const path = require('path');
+
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express(); // instatntiating the server
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require('./data/animals'); 
 
 
@@ -68,6 +78,40 @@ app.get('/api/animals', (req, res) => {
 });
 
 //==========================================================
+// validating data
+//==========================================================
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+//==========================================================
+// creating a function to handle taking the data from req.body and adding it to our animals.json file.
+//==========================================================
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  return animal;
+}
+
+//==========================================================
 // creating a get route with req.params object *** ref. 11.1.7
 //==========================================================
 app.get('/api/animals/:id', (req, res) => {
@@ -76,6 +120,22 @@ app.get('/api/animals/:id', (req, res) => {
     res.json(result);
   } else {
     res.send(404);
+  }
+});
+
+//==========================================================
+// creating a route so users can input info *** ref. 11.2.6
+//==========================================================
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
   }
 });
 
